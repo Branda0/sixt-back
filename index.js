0b2ce488bc13ae1c02beac2b604545583c5e85d0
Branda0 <g.brandao.da.cunha@gmail.com>
2022-03-20 15:43:31 +0100

    switched mongoDB to distant DB

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(cors());
 app.use(formidable());
 
-// mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
-mongoose.connect("mongodb://localhost/sixt");
+// mongoose.connect("mongodb://localhost/sixt");
 
 const agenciesRoutes = require("./routes/agencies");
 app.use(agenciesRoutes);
